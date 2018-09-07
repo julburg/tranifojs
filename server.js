cdc5bb7d7add0002a3c56f36ab1e2c41e4f96b0a
Julia Burgard <burgard@synyx.de>
2018-09-07 09:21:02 +0200

    Add some more routes

--- server.js
+++ server.js
@@ -63,5 +63,29 @@ app.listen(3000, function () {
 var RouteEnum = {
     ONE: {name: "1"},
     TWO: {name: "2"},
-    THREE: {name: "3"}
-};
+    THREE: {name: "3"},
+    FOUR: {name: "4"},
+    FIVE: {name: "5"},
+    SIX: {name: "6"},
+    EIGHT: {name: "8"},
+
+    S1: {name: "S1"},
+    S11: {name: "S11"},
+    S2: {name: "S2"},
+    S3: {name: "S3"},
+    S31: {name: "S31"},
+    S32: {name: "S32"},
+    S4: {name: "S4"},
+    S41: {name: "S41"},
+    S42: {name: "S42"},
+    S5: {name: "S5"},
+    S51: {name: "S51"},
+    S52: {name: "S52"},
+    S6: {name: "S6"},
+    S7: {name: "S7"},
+    S71: {name: "S71"},
+    S8: {name: "S8"},
+    S81: {name: "S81"},
+    S9: {name: "S9"}
+}
+;
